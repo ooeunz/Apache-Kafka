35d4ac212a7ca3b81f1985bde05d8ccb9c685de5
ooeunz <yuns994@gmail.com>
2020-07-27 17:50:46 +0900

    Kafka consumer sample

---
+++ src/main/java/com/sample/kafka/consumer/ConsumerBasic.java
@@ -0,0 +1,34 @@
+package com.sample.kafka.consumer;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+
+import java.util.Arrays;
+import java.util.Properties;
+
+public class ConsumerBasic {
+    public void receiveFromKafka() {
+        Properties props = new Properties();
+        props.put("bootstrap.servers", "127.0.0.1:9092");
+        props.put("group.id", "basic-consumer");
+        props.put("enable.auto.commit", "true");
+        props.put("auto.offset.reset", "latest");
+        props.put("key.deserializer", "org.apache.kafka.common.serialization");
+        props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<String, String>(props);
+        consumer.subscribe(Arrays.asList("basic-consumer"));
+        try {
+            while (true) {
+                ConsumerRecords<String, String> records = consumer.poll(100);
+                for (ConsumerRecord<String, String> record : records) {
+                    System.out.printf("Topic: %s, Partition: %s, Offset: %d, Key: %s, Value: %n\n",
+                            record.topic(), record.partition(), record.offset(), record.key(), record.value());
+                }
+            }
+        } finally {
+            consumer.close();
+        }
+    }
+}
